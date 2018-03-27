5c012ab19883b79b9bd4b37d8364f468a6133310
Chelsea Neto Fernandes <chelsea.fernandes@ocsbstudent.ca>
2018-03-27 10:50:40 -0400

    Added another heart

--- main.lua
+++ main.lua
@@ -10,14 +10,28 @@
 display.setStatusBar(display.HiddenStatusBar)
 
 -- variable for the timer
-local totalSeconds = 5
-local secondsLeft = 5
+local totalSeconds = 10
+local secondsLeft = 10
 local clockText
 local countDownTimer
 
 local lives = 3
 local heart1
 local heart2
+local heart3
+local heart4
+local gameOver
+
+local questionObject
+local correctObject
+local numericField 
+local randomNumber1
+local randomNumber2
+local userAnswer
+local correctAnswer
+local incorrectAnswer
+local incorrectObject
+local randomOperator
 
 --*** ADD LOCAL VARIABLE FOR: INCORRECT OBJECT, POINTS OBJECT, POINTS
 
@@ -25,8 +39,45 @@ local heart2
 ----------------------------------------------------------------------------------
 --LOCAL FUNCTIONS
 ----------------------------------------------------------------------------------
+local function AskQuestion()
+	-- generate 2 random numbers between a max. and a min. number
+	randomNumber1 = math.random(-5, 20)
+	randomNumber2 = math.random(-5, 20)
 
+	correctAnswer = randomNumber1 + randomNumber2
 
+-- create question in text object
+	questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
+
+end
+
+local function HideCorrect()
+	correctObject.isVisible = false 
+
+end
+---------------------------------------------------------------------------
+ local function NumericFieldListener( event )
+
+ 	-- User begins editing "numericField"
+ 	if ( event.phase == "began" ) then
+
+ 		-- clear text field
+ 		event.target.text = ""
+
+ 	elseif event.phase == "submitted" then
+
+ 		-- when the answer is submitted (enter key is pressed) set user input to user's answer
+ 		userAnswer = tonumber (event.target.text)
+
+ 		-- if the users answer and the correct answer are the same:
+ 		if (userAnswer == correctAnswer) then
+ 			correctObject.isVisible = true
+ 			timer.performWithDelay(2000, HideCorrect)
+ 		end
+ 	end
+ end
+
+----------------------------------------------------------------------------
 local function UpdateTime()
 
 	-- decrement the number of seeconds
@@ -39,16 +90,6 @@ local function UpdateTime()
 		-- reset the number of seconds left
 		secondsLeft = totalSeconds
 		lives = lives - 1
-
-		--*** IF THERE ARE NO LIVES LEFT, PLAY A LOSE SOUND, SHOW A YOU LOSE IMAGE
-		-- AND CANCEL THE TIMER REMOVE THE THIRD HEART BY MAKING IT INVISBLE
-		if (lives == 2) then
-			heart2.isVisible = false
-		elseif (lives == 1) then
-				heart1.isVisible = false
-		end
-
-		--*** CALL THE FUNCTION TO ASK A NEW QUESTION
 	end
 end
 
@@ -59,8 +100,100 @@ local function StarterTimer()
 end
 
 --------------------------------------------------------------------------------
+local function UpdateHearts()
+	if (lives == 4 ) then
+		heart1.isVisible = true
+		heart2.isVisible = true
+		heart3.isVisible = true
+		heart4.isVisible = true
+	elseif (lives == 3 ) then 
+			heart1.isVisible = false
+			heart2.isVisible = true
+			heart3.isVisible = true
+			heart4.isVisible = true
+	elseif (lives == 2 ) then 
+			heart1.isVisible = false
+			heart2.isVisible = false
+			heart3.isVisible = true
+			heart4.isVisible = true
+	elseif (lives == 1 ) then 
+			heart1.isVisible = false
+			heart2.isVisible = false
+			heart3.isVisible = false
+			heart4.isVisible = true
+	elseif (lives == 0 ) then 
+			heart1.isVisible = false
+			heart2.isVisible = false
+			heart3.isVisible = false
+			heart4.isVisible = false
+			gameOver.isVisible = true
+		
+	end
+end
+
+--------------------------------------------------------------------------------
 --OBJECT CREATION
 ---------------------------------------------------------------------------------
-
 -- create the lives to display on the screen
-heart1 = display.newImageRect("Image/heart.png", 100, 100)
+heart4 = display.newImageRect("Images/heart.png", 100, 100)
+heart4.x = display.contentWidth * 7 / 8
+heart4.y = display.contentHeight * 1 / 7
+
+heart3 = display.newImageRect("Images/heart.png", 100, 100)
+heart3.x = display.contentWidth * 6 / 8
+heart3.y = display.contentHeight * 1 / 7
+
+heart2 = display.newImageRect("Images/heart.png", 100, 100)
+heart2.x = display.contentWidth * 5 / 8
+heart2.y = display.contentHeight * 1 /7
+
+heart1 = display.newImageRect("Images/heart.png", 100, 100)
+heart1.x = display.contentWidth * 4 / 8
+heart1.y = display.contentHeight * 1 / 7
+
+
+
+---------------------------------------------------------------------------------
+-- displays a question and sets the colour
+questionObject = display.newText( "", display.contentWidth/3, display.contentHeight/2, nil, 50 )
+questionObject:setTextColor(255/255, 255/255, 0/255)
+
+-- create the correct text object and make it invisible
+correctObject = display.newText( "Correct!", display.contentWidth/2, display.contentHeight*2/3, nil, 50 )
+correctObject:setTextColor(255/255, 250/255, 0/255)
+correctObject.isVisible = false
+
+-- create the incorrect text object and make it invisible
+incorrectObject = display.newText( "Incorrect!", display.contentWidth/2, display.contentHeight*2/3, nil, 50 )
+incorrectObject:setTextColor(155/255, 42/255, 198/255)
+
+incorrectObject.isVisible = false
+
+-- Create numeric field
+numericField = native.newTextField( display.contentWidth/2, display.contentHeight/2, 150, 80 )
+numericField.inputType =  "number"
+
+-- add the event listener for the numeric field
+numericField:addEventListener( "userInput", NumericFieldListener )
+
+-------------------------------------------------------------------
+-- FUNCTION CALLS
+------------------------------------------------------------------
+
+-- call the funtion to ask the question
+AskQuestion()
+
+-- call the function to update hearts
+UpdateHearts()
+
+-- call the function to  update the timer
+UpdateTime()
+
+-- call the function to call the timer
+StarterTimer()
+
+-- call the function to hide the correct answer
+HideCorrect()
+
+-- call the function to numeric field listener
+NumericFieldListener()
